b4fa07b2828496af02a9adee705666d9d62970bb
Gabriel Brandão <deostulti2@gmail.com>
2024-11-01 22:36:49 +0000

    Remove PHPUnit from project

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -31,18 +31,6 @@ class ComposerAutoloaderInitd04a6a4216eec9684d7a430e94632a78
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInitd04a6a4216eec9684d7a430e94632a78::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
-
         return $loader;
     }
 }
